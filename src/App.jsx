2cc33a7154438e040c62682ff26ceff4aee0dd0e
Yosef Rezaie <yosefrezaie5001@gmail.com>
2025-01-08 08:33:28 +0330

    Project routing with spa

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import DetailsPage from "./pages/DetailsPage";
+import CheckoutPage from "./CheckoutPage";
+import PagesNotFound from "./pages/PagesNotFound";
 
 function App() {
-
   return (
-    <div></div>
-  )
+    <Routes>
+      <Route path="/" element={<Navigate to="/products" />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<DetailsPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/*" element={<PagesNotFound />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
