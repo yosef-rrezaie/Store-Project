72366db484f039d9fcffc636a4583b96dbdba953
Yosef Rezaie <yosefrezaie5001@gmail.com>
2025-01-09 08:08:04 +0330

    Add loader

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useProducts } from "../context/ProductsProvider";
 import styles from "./ProductsPage.module.css";
 import Card from "../component/Card";
+import Loader from "../component/Loader";
 
 function ProductsPage() {
   const products = useProducts();
   return (
     <div className={styles.container}>
       <div className={styles.products}>
-        {products.length ? null : <p>Loading</p>}
+        {products.length ? null : <Loader />}
         {products.map((p) => (
           <Card key={p.id} data={p} />
         ))}
