b079ae110b43bd3209ed8ae6207b89607e4df9c6
Yosef Rezaie <yosefrezaie5001@gmail.com>
2025-01-27 06:13:58 +0330

    layout completed and style added

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { PiShoppingCartSimpleBold } from "react-icons/pi";
+import { useCart } from "../context/CartProvider";
+import styles from "./Layout.module.css";
 
 function Layout({ children }) {
+  const [state] = useCart();
   return (
     <>
-      <header>
-        <p>hello</p>
+      <header className={styles.header}>
+        <Link to="/products">555</Link>
+        <Link to="/checkout">
+          <div>
+          <PiShoppingCartSimpleBold />
+          {!!state.itemsCounter && <span>{state.itemsCounter}</span>}
+          </div>
+        </Link>
       </header>
       {children}
-      <footer></footer>
+      <footer className={styles.footer}>
+        <p>Developed by Yosef Rezaie with love</p>
+      </footer>
     </>
   );
 }
